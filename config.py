1c8cc9070d6e79fb7c7c2dcd8af4a02866e8fdc3
akashchavan95 <apchavan95@gmail.com>
2022-10-18 17:28:46 +0530

    add all files

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+MODEL_FILE_PATH = os.path.join("project_folder","KNN_REG_MODEL.pkl")
+SCALING_FILE_PATH = os.path.join("project_folder","Scaling_std.pkl")
+JSON_FILE_PATH = os.path.join("project_folder","Columns_Data.json")
+PORT_NUMBER = 5008
---
+++ j.py
@@ -0,0 +1,7 @@
+from gettext import install
+import json 
+import config
+with open(config.JSON_FILE_PATH,"r") as file:
+    json_data = json.load(file)
+print(len(json_data["Columns"]))
+
